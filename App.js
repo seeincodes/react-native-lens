3434f7707929948742656e419432e9325abfa019
“seeinplays” <seeinplays@gmail.com>
2022-12-25 12:10:03 -0700

    Profiles view

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
+import { Profiles } from '@lens-protocol/react-native-lens-ui-kit';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Profiles" component={ProfileComponent} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
+}
+
+function ProfileComponent({
+  navigation
+}) {
+  return (
+    <Profiles />
+  )
 }
 
 const styles = StyleSheet.create({
